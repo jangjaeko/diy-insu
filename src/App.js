9f1e462cd97d725987c19830b06c65b710386b46
jangjaeko <jangjaeko45@gmail.com>
2021-07-09 00:17:46 +0900

    add Carousel

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Suspense } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Switch } from "react-router-dom";
 import Navbar from "./component/view/Navigationbar";
+import ImageSlider from "./component/view/ImageSlider";
 export default function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Navbar />
+      <ImageSlider />
     </Suspense>
   );
 }
---
+++ src/component/view/ImageSlider.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import image1 from "./images/1.jpg";
+import image2 from "./images/2.jpg";
+import image3 from "./images/3.jpg";
+export default function ImageSlider() {
+  return (
+    <Carousel>
+      <Carousel.Item interval={1000}>
+        <img
+          className="d-block w-100"
+          style={{ height: "40vh" }}
+          src={image1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={500}>
+        <img
+          className="d-block w-100 "
+          style={{ height: "40vh" }}
+          src={image2}
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          style={{ height: "40vh" }}
+          src={image3}
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
